dab7931ba8a140fa97c4c371786506a71d70ae99
jlmoer <jlmoerdijk@gmail.com>
2022-01-26 14:57:23 +0100

    first commit users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import users from './users.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        {users.map((user) =>{
+          return(
+            <p>{user.name} - {user.email} - {user.website}</p>
+          )
+        })}
+      </div>
+    )
+  }
+
 }
 
+  
+
+
+
 export default App;
+
+
+
+
